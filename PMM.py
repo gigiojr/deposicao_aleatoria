fc45c2812946ec90a2684f46e0c4d0a102eee8d8
Giovanni Amormino <gigio_jr@hotmail.com>
2018-11-19 13:08:40 -0200

    First commit

---
+++ PMM.py
@@ -0,0 +1,78 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def buscaLateral(lado, pos, linha, block):
+    if lado:  # direita
+        if pos < len(linha) - 1:
+            if linha[pos + 1] == 0:
+                pos += 1
+            elif pos+1 < len(linha) - 1:
+                if linha[pos + 2] == 0:
+                    pos += 2
+                else:
+                    if block:
+                        pos = buscaLateral(0, pos, linha, 0)
+            else:
+                if block:
+                    pos = buscaLateral(0, pos, linha, 0)
+        else:
+            if block:
+                pos = buscaLateral(0, pos, linha, 0)
+    else:  # esquerda
+        if pos > 0:
+            if linha[pos - 1] == 0:
+                pos -= 1
+            elif pos-1 > 0:
+                if linha[pos - 2] == 0:
+                    pos -= 2
+                else:
+                    if block:
+                        pos = buscaLateral(1, pos, linha, 0)
+            else:
+                if block:
+                    pos = buscaLateral(1, pos, linha, 0)
+        else:
+            if block:
+                pos = buscaLateral(1, pos, linha, 0)
+    return pos
+
+
+def makeDA(maxAlt, l, t):  # Deposição Aleatória
+    vet = np.zeros((maxAlt, l), int)
+    for i in range(t):
+        valRnd = random.randint(0, l - 1)
+        for j in range(maxAlt):
+            if vet[j][valRnd] == 0:
+                vet[j][valRnd] = 1
+                break
+    return vet
+
+
+# Deposição Aleatória com Relaxamento Superficial
+def makeDARS(maxAlt, l, t):
+    vet = np.zeros((maxAlt, l), int)
+    for i in range(t):
+        valRnd = random.randint(0, l - 1)
+        for j in range(maxAlt):
+            if vet[j][valRnd] == 0:
+                vet[j][valRnd] = 1
+                break
+            else:
+                lado = random.randint(0, 1)
+                pos = buscaLateral(lado, valRnd, vet[j][:], 1)
+                if pos != valRnd:
+                    vet[j][pos] = 1
+                    break
+    return vet
+
+
+vetDA = makeDA(200, 200, 10 ** 4)
+vetDARS = makeDARS(200, 200, 10 ** 4)
+
+plt.figure(0)
+plt.imshow(vetDA, origin='lower')
+plt.figure(1)
+plt.imshow(vetDARS, origin='lower')
+plt.show()
